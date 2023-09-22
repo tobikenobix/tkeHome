23953cff450949efbf7541edcb163ecf1def0d4b
Tobi <tobias.schneider@web.de>
2023-09-22 17:33:19 +0200

    added types to enum

--- Semester2/basicsOO/virtualpet/src/main/java/virtualpets/PetType.java
+++ Semester2/basicsOO/virtualpet/src/main/java/virtualpets/PetType.java
@@ -6,4 +6,35 @@ package virtualpets;
  * @version 0.1
  */
 public enum PetType {
+    DOG(2,2,2),
+    CAT(1,3,2),
+    BIRD(0.75,1,0.75),
+    RABBIT(0.5,0.5,1);
+
+    private final double happinesMultiplier;
+    private final double hungrinessMultiplier;
+    private final double sleepinessMultiplier;
+
+    /**
+     * Constructor creates those four animales
+     */
+    private PetType(double happinesMultiplier, double hungrinessMultiplier, double sleepinessMultiplier){
+        this.happinesMultiplier=happinesMultiplier;
+        this.hungrinessMultiplier=hungrinessMultiplier;
+        this.sleepinessMultiplier=sleepinessMultiplier;
+    }
+
+    //getter block
+
+    public double getHappinesMultiplier() {
+        return happinesMultiplier;
+    }
+
+    public double getHungrinessMultiplier() {
+        return hungrinessMultiplier;
+    }
+
+    public double getSleepinessMultiplier() {
+        return sleepinessMultiplier;
+    }
 }
