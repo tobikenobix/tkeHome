3c5b741237d591ca39456d3ec385b0187defc629
Tobi <tobias.schneider@web.de>
2023-09-25 11:19:12 +0200

    completed ChatChepyT with handling functions

--- Semester2/arraysAndStrings/chatcheapyt/src/main/java/ChatCheapyT.java
+++ Semester2/arraysAndStrings/chatcheapyt/src/main/java/ChatCheapyT.java
@@ -10,57 +10,76 @@ public class ChatCheapyT{
   }
 
   private static String handleTooLong(String input){
-
-    // TODO
-    return null;
+    if (input.length()>50)
+      return "Das ist mir zuviel zu lesen! Bitte kürzen Sie Ihre Anfrage!";
+    else
+      return null;
 
   }
 
   private static String handleExam(String input){
-
-    // TODO
-    return null;
+    if(input.equals("Was kommt in der Klausur dran?"))
+      return "Die Klausur orientiert sich an den Praktika!";
+    else
+      return null;
 
   }
 
   private static String handleQuestion(String input){
-
-    // TODO
+    if(input.endsWith("?"))
+      return "Tut mir leid, aber die ChatCheapyT-Server sind gerade ausgelastet! Schließen Sie bitte ein ChatCheapyT-Pro-Abo ab!";
     return null;
 
   }
 
   private static String handleExclamation(String input){
-
-    // TODO
+    if (input.contains("!")){
+      if(input.toLowerCase().contains("bitte"))
+        return "Als Antwort habe ich ein YouTube-Video generiert: https://youtu.be/dQw4w9WgXcQ";
+      else
+        return "Wie ist das Zauberwort?";
+    }
     return null;
 
   }
 
   private static String handleChatGPT(String input){
-
-    // TODO
+    if(input.contains("ChatGPT")){
+      return input.replace("ChatGPT", "ChatCheapyT");
+    }
     return null;
 
   }
 
   private static String handleScream(String input){
-
-    // TODO
+    int countUpper=0;
+    for(int i=0; i < input.length(); i++){
+      if(Character.isUpperCase(input.charAt(i))) countUpper++;
+    }
+    if(countUpper>=input.length()/2)
+      return "Bitte schreien Sie mich nicht an!";
     return null;
 
   }
 
   private static String handleReverse(String input){
-
-    // TODO
+    if(input.startsWith("Umdrehen:")){
+      StringBuffer sb = new StringBuffer(input.split(":",2)[0]);
+      return sb.reverse().toString();
+    }
     return null;
 
   }
 
   private static String handleAdd(String input){
+    if(input.toLowerCase().startsWith("addiere")){
+      String[] nums =input.split(" ");
+      if(nums.length >3) {
+          return "Nur zwei Zahlen erlaubt";
+      }
+      return String.format("%s plus %s ist gleich %f! Take that, ChatGPT",nums[1],nums[2],Double.parseDouble(nums[1])+Double.parseDouble(nums[2]));
+    }
 
-    // TODO
     return null;
 
   }
