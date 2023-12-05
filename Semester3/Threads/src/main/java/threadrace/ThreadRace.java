c0664613d6496cb937735454a51bcc34face8ff2
Tobi <tobias.schneider@web.de>
2023-12-05 18:29:16 +0100

    finished first exercise

--- Semester3/Threads/src/main/java/threadrace/ThreadRace.java
+++ Semester3/Threads/src/main/java/threadrace/ThreadRace.java
@@ -1,4 +1,30 @@
 package threadrace;
 
 public class ThreadRace {
+    private static int raceCounter;
+    private static void threadHelper(int adder, Object locker){
+        while(!Thread.interrupted()) {
+            fib(35);
+            synchronized (locker) {
+                raceCounter += adder;
+            }
+        }
+    }
+    public static void main(String[] args) throws InterruptedException {
+        final Object locker= new Object();
+        raceCounter = 0;
+        Thread threadA = new Thread(()->threadHelper(1, locker));
+        Thread threadB = new Thread(()-> threadHelper(-1, locker));
+        Thread.sleep(10000);
+        threadA.interrupt();
+        threadB.interrupt();
+        threadA.join();
+        threadB.join();
+        System.out.println(raceCounter);
+
+    }
+
+    private static int fib(int n) {
+        return (n <= 1 ? n : fib(n - 1) + fib(n - 2));
+    }
 }
